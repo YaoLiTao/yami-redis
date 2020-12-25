c39d277265530a49726a8eda8f12b6a6573a0fe1
yaolitao <1738642116@qq.com>
2020-12-25 23:23:32 +0800

    update

--- src/main/java/cache/DictType.java
+++ src/main/java/cache/DictType.java
@@ -10,61 +10,64 @@ public class DictType<K> {
     private static final int c1 = 0xcc9e2d51;
     private static final int c2 = 0x1b873593;
     private static final int r1 = 15;
-    private static final int r2 = 15;
+    private static final int r2 = 13;
     private static final int m = 5;
     private static final int n = 0xe6546b64;
 
     // MurmurHash_32
-    public int hashFunction(byte[] key, long seed) {
-        long hash = seed;
+    public int hashFunction(byte[] key, int seed) {
+        int hash = seed;
         int len = key.length;
 
         int remainingBytesCount = len % 4;
         int fourByteChunkCount = len - remainingBytesCount;
-        long k;
+
+        // 整倍数阶段
         int i = 0;
         for (; i < fourByteChunkCount; i += 4) {
-            k = key[i];
+            // 小端序把每4字节转为int
+            int k = 0;
+            k |= key[i + 3] & 0xFF;
             k <<= 8;
-            k |= key[i + 1];
+            k |= key[i + 2] & 0xFF;
             k <<= 8;
-            k |= key[i + 2];
+            k |= key[i + 1] & 0xFF;
             k <<= 8;
-            k |= key[i + 3];
+            k |= key[i] & 0xFF;
 
+            // 混淆
             k *= c1;
-            k = (k << r1) | (k >> (32 - r2));
+            k = (k << r1) | (k >>> (32 - r1));
             k *= c2;
 
+            // 整倍数阶段计算
             hash ^= k;
-            hash = (hash << r2) | (hash >> (32 - r2));
+            hash = (hash << r2) | (hash >>> (32 - r2));
             hash = hash * m + n;
         }
 
-        long remainingBytes = 0;
-        for (int j = remainingBytesCount - 1; j >= 0; j--) {
-            remainingBytes = key[i + j];
-            remainingBytes <<= 8;
+        // 余数阶段
+        int r = 0;
+        if (remainingBytesCount > 0) {
+            for (int j = 0; j < remainingBytesCount; j++) {
+                r <<= 8;
+                r |= key[i + j] & 0xFF;
+            }
+            r *= c1;
+            r = (r << r1) | (r >>> (32 - r1));
+            r *= c2;
+
+            hash ^= r;
         }
-        remainingBytes *= c1;
-        remainingBytes = (remainingBytes << r1) | (remainingBytes >> (32 - r1));
-        remainingBytes *= c2;
 
-        hash ^= remainingBytes;
         hash ^= len;
-        hash ^= (hash >> 16);
+
+        hash ^= (hash >>> 16);
         hash *= 0x85ebca6b;
-        hash ^= (hash >> 13);
+        hash ^= (hash >>> 13);
         hash *= 0xc2b2ae35;
-        hash ^= (hash >> 16);
-
-        return (int) hash;
-    }
-
-    public static void main(String[] args) {
-        byte[] bytes = "ABCDE".getBytes(StandardCharsets.US_ASCII);
-        System.out.println(new DictType<>().hashFunction(bytes, 0));
-        System.out.println(Hashing.murmur3_32().hashBytes(bytes).asInt());
+        hash ^= (hash >>> 16);
+        return hash;
     }
 
 }
